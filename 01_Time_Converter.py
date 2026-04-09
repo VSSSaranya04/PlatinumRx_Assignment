eadf8243cc9a56983283827c74377df64cd38338
Sri Sai Saranya Vulli <saisaranyavulli06@gmail.com>
2026-04-09 13:48:07 +0530

    Python
    Added Python scripts

---
+++ 01_Time_Converter.py
@@ -0,0 +1,12 @@
+def convert_minutes(minutes):
+    hours = minutes // 60
+    mins = minutes % 60
+
+    if hours == 1:
+        return f"{hours} hr {mins} minutes"
+    else:
+        return f"{hours} hrs {mins} minutes"
+
+# Example
+print(convert_minutes(130))
+print(convert_minutes(110))
